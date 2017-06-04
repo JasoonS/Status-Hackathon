8b43aff81a1343e7429f8d5db2da36d36e954700
Jason Smythe <jason@smythemail.za.net>
2017-06-04 04:48:40 +0200

    Basic creation of group working in JS.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import PeerCoinContract from '../build/contracts/PeerCoin.json'
 import Config from '../truffle.js'
 import Web3 from 'web3'
-
-import './css/oswald.css'
-import './css/open-sans.css'
-import './css/pure-min.css'
-import './App.css'
 
 class App extends Component {
   constructor(props) {
     super(props)
 
+    this.createGroup = this.createGroup.bind(this)
+    this.setInputId = this.setInputId.bind(this)
+    this.setInputGroup = this.setInputGroup.bind(this)
+
     this.state = {
-      storageValue: 0
+      groupNameInput: '',
+      groupIdInput: ''
     }
   }
 
   componentWillMount() {
+  }
+
+  createGroup() {
+    console.log('create group')
     var self = this
 
     var {host, port} = Config.networks[process.env.NODE_ENV]
@@ -25,60 +29,66 @@ class App extends Component {
                       new Web3.providers.HttpProvider('http://' + host + ':' + port)
                       : window.web3.currentProvider
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(provider)
+    const peerCoin = contract(PeerCoinContract)
+    peerCoin.setProvider(provider)
 
-    // Get Web3 so we can get our accounts.
     const web3RPC = new Web3(provider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    var peerCoinInstance
 
-    // Get accounts.
+    console.log('before get accounts')
     web3RPC.eth.getAccounts(function(error, accounts) {
-      console.log(accounts)
-
-      simpleStorage.deployed().then(function(instance) {
-        simpleStorageInstance = instance
-
-        // Stores a value of 5.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then(function(result) {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then(function(result) {
-        // Update state with the result.
-        return self.setState({ storageValue: result.c[0] })
+      console.log('in get accounts', accounts)
+      peerCoin.deployed().then(function(instance) {
+        peerCoinInstance = instance
+        console.log('adding',self.state.groupIdInput, self.state.groupNameInput)
+        return peerCoinInstance.addGroup(self.state.groupIdInput, self.state.groupNameInput, {from: accounts[0]})
+      }).then(function(a) {
+        peerCoinInstance.getGroupName(self.state.groupIdInput).then(function(result) {
+          console.log( window.web3.toAscii(result))
+        })
       })
+    })
+    console.log('after get accounts')
+  }
+
+  setInputGroup(e) {
+    this.setState({
+      ...this.state,
+      groupNameInput: e.target.value
+    })
+  }
+
+  setInputId(e) {
+    this.setState({
+      ...this.state,
+      groupIdInput: e.target.value
     })
   }
 
   render() {
     return (
       <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-            {/*}<ul className="pure-menu-list">
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">News</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Sports</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Finance</a></li>
-            </ul>*/}
-        </nav>
-
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>The below will show a stored value of 5 by default if your contracts compiled and migrated successfully.</p>
-              <p>Try changing the value stored on <strong>line 50</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
-            </div>
-          </div>
-        </main>
+        <div>
+          <h1>Create Group:</h1>
+          <input placeholder={'Group Name'} value={this.state.groupNameInput} onChange={this.setInputGroup}/>
+          <input placeholder={'Group ID'} value={this.state.groupIdInput} onChange={this.setInputId}/>
+          <button onClick={this.createGroup}>Create Group</button>
+        </div>
       </div>
-    );
+    )
+    // <table>
+    // <thead>
+    // <tr>
+    // <th>First Name</th>
+    // <th>Last Name</th>
+    // <th>Age</th>
+    // </tr>
+    // </thead>
+    // <tbody>
+    // {peerCoin}
+    // </tbody>
+    // </table>
   }
 }
 

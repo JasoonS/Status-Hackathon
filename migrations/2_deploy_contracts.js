e541a5eaf6248db0c2623f84b7f311fe86f0d12d
Brendan <brendan@southeasterdesign.com>
2017-06-04 16:46:13 +0200

    Commented out people and groups.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var PeerCoin = artifacts.require("./PeerCoin.sol");
-var People = artifacts.require("./People.sol");
-var User = artifacts.require("./User.sol");
+// var People = artifacts.require("./People.sol");
+// var User = artifacts.require("./User.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(PeerCoin);
-  deployer.deploy(People);
-  deployer.deploy(User);
+  // deployer.deploy(People);
+  // deployer.deploy(User);
 };

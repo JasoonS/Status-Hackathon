6c0d3b66c962756bbd6e711bd3d99c303ca78621
Jason Smythe <jason@smythemail.za.net>
2017-06-08 15:00:36 +0200

    Add user feedback for create group input.

--- src/containers/CreateGroup.js
+++ src/containers/CreateGroup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 import { connect } from 'react-redux'
+import {loadUsersGroups} from '../actions'
 
 class CreateGroup extends Component {
   constructor(props) {
@@ -17,7 +18,10 @@ class CreateGroup extends Component {
       groupIdInput: '',
       tokenName: '',
       tokenSymbol: '',
-      isIdAvailable: true
+      isIdAvailable: true,
+      nameRequiredWarn: false,
+      idRequiredWarn: false,
+      tokenRequiredWarn: false
     }
   }
 
@@ -28,14 +32,16 @@ class CreateGroup extends Component {
       ...this.state,
       groupNameInput: e.target.value,
       groupIdInput: newGroupId,
-      isIdAvailable: changeId? true : this.state.isIdAvailable // if the ID doesn't change don't change this
+      isIdAvailable: changeId? true : this.state.isIdAvailable, // if the ID doesn't change don't change this
+      nameRequiredWarn: false,
+      idRequiredWarn: changeId? false : this.state.idRequiredWarn //TODO:: (silly edge case bug here...)changeId? this.state.idRequiredWarn : false
     })
     if (changeId) {
       this.props.peerCoinInstance.isGroupIdUsed(newGroupId).then(result => {
         console.log('is the id available?', result)
         this.setState({
           ...this.state,
-          isIdAvailable: !result
+          isIdAvailable: !result,
         })
       })
     }
@@ -44,20 +50,23 @@ class CreateGroup extends Component {
     this.setState({
       ...this.state,
       groupIdInput: e.target.value,
-      isIdAvailable: true
+      isIdAvailable: true,
+      idRequiredWarn: false
     })
     this.props.peerCoinInstance.isGroupIdUsed(e.target.value).then(result => {
       console.log('is the id available?', result)
       this.setState({
         ...this.state,
-        isIdAvailable: !result
+        isIdAvailable: !result,
+        idRequiredWarn: false
       })
     })
   }
   setTokenName(e) {
     this.setState({
       ...this.state,
-      tokenName: e.target.value
+      tokenName: e.target.value,
+      tokenRequiredWarn: false
     })
   }
   setTokenSymbol(e) {
@@ -67,48 +76,82 @@ class CreateGroup extends Component {
     })
   }
   render() {
-    let createGroup = () =>   this.props.dispatch(
-        loadUsersGroups(this.props.peerCoinInstance, this.setGroupName, this.setGroupID, this.setTokenName)
-      )
     const {
       groupNameInput,
       groupIdInput,
       tokenName,
       tokenSymbol,
-      isIdAvailable
+      isIdAvailable,
+      nameRequiredWarn,
+      idRequiredWarn,
+      tokenRequiredWarn
     } = this.state
+    let createGroup = () => {
+      let nameRequiredWarn = false
+      let idRequiredWarn = false
+      let tokenRequiredWarn = false
+      if (groupNameInput === '') {
+        nameRequiredWarn = true
+      }
+      if (groupIdInput === '') {
+        idRequiredWarn = true
+      }
+      if (tokenName === '') {
+        tokenRequiredWarn = true
+      }
+      this.setState({
+        ...this.state,
+        idRequiredWarn,
+        tokenRequiredWarn,
+        nameRequiredWarn
+      })
+      if (!(nameRequiredWarn || nameRequiredWarn || nameRequiredWarn)){
+        this.props.peerCoinInstance.isGroupIdUsed(groupIdInput).then(result => {
+          console.log('is the id available?', result)
+          this.setState({
+            ...this.state,
+            isIdAvailable: !result
+          })
+        })
+        // this.props.dispatch(
+        //   loadUsersGroups(this.props.peerCoinInstance, this.setGroupName, this.setGroupID, this.setTokenName)
+        //   // TODO: show loader here!
+      }
+    }
     return (
-      <div className="CreateGroup">
+      <div className='CreateGroup'>
         <h1>CreateGroup</h1>
 
         <TextField
-          floatingLabelText="Group Name"
+          floatingLabelText='Group Name'
           onChange={this.setGroupName}
           value={groupNameInput}
+          errorText={nameRequiredWarn? 'Group Name is a required field.' : ''}
           fullWidth={true}
         />
 
         <TextField
-          floatingLabelText="Group ID - this will be used to uniquely identify the group."
+          floatingLabelText='Group ID - this will be used to uniquely identify the group.'
           onChange={this.setGroupID}
           value={groupIdInput}
-          errorText={isIdAvailable? "" : "Sorry the ID " + groupIdInput + " is unavailable"}
+          errorText={idRequiredWarn? 'Token ID is a required field' : (isIdAvailable? '' : 'Sorry the ID ' + groupIdInput + ' is unavailable') }
           fullWidth={true}
         />
 
         <TextField
-          floatingLabelText="Token Name"
+          floatingLabelText='Token Name'
           onChange={this.setTokenName}
           value={tokenName}
+          errorText={tokenRequiredWarn?  'Token Name is a required field.' : ''}
           fullWidth={true}
         />
 
         {/*<TextField
-          floatingLabelText="Group ID - this will be used to uniquely identify the group."
+          floatingLabelText='Group ID - this will be used to uniquely identify the group.'
           onChange={groupIdInput}
         />*/}
 
-        <RaisedButton onClick={createGroup} label="Create Group" primary={true} fullWidth={true}/>
+        <RaisedButton onClick={createGroup} label='Create Group' primary={true} fullWidth={true}/>
       </div>
     )
   }
